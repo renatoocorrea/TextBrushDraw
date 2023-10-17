4d7b26a43051948b48327ce1bde79580dd9f2627
Renato Corrêa <renato.correa@venturus.org.br>
2023-10-17 14:00:54 -0300

    initial commit.

--- app/src/main/java/com/renatoocorrea/textbrush/MainActivity.kt
+++ app/src/main/java/com/renatoocorrea/textbrush/MainActivity.kt
@@ -1,32 +1,58 @@
 package com.renatoocorrea.textbrush
 
 import android.R.attr.path
-import android.graphics.Path
 import android.graphics.RectF
 import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.changedToDown
+import androidx.compose.ui.input.pointer.changedToDownIgnoreConsumed
+import androidx.compose.ui.input.pointer.consumeDownChange
+import androidx.compose.ui.input.pointer.consumePositionChange
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.renatoocorrea.textbrush.ui.theme.TextBrushTheme
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 
 class MainActivity : ComponentActivity() {
@@ -37,12 +63,14 @@ class MainActivity : ComponentActivity() {
         )
 
     val state: Flow<Path> = mutableState
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val path = Path()
+       /* val path = Path()
         val auxX = 411
         val auxY = 656
-        path.lineTo(auxX.toFloat(), auxY.toFloat())
+        path.lineTo(auxX.toFloat(), auxY.toFloat())*/
+        val path = Path()
         setContent {
             TextBrushTheme {
                 // A surface container using the 'background' color from the theme
@@ -50,32 +78,323 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    DrawingApp()
                     /*Box(modifier = Modifier.fillMaxSize()) {
 //                    Greeting("Android")
                         ArcTextExample()
                     }*/
-                    Box(
+                    /*Box(
                         Modifier.pointerInput(Unit) {
-                                detectDragGestures { change, dragAmount ->
-
+                            detectDragGestures { change, dragAmount ->
+                                if (change.changedToDownIgnoreConsumed()) {
                                     val x = change.position.x
                                     val y = change.position.y
                                     Log.e("TESTE", "X: " + x)
                                     Log.e("TESTE", "Y: " + y)
-                                    path.lineTo(x,y)
-
+                                    path.lineTo(x, y)
+                                    mutableState.update {
+                                        Log.e("TESTE", "UPDATED: " + it.toString())
+                                        (Path(path))
+                                    }
+                                } else {
+                                    change.consume()
+                                }
 
 //                                    change.consume()
-                                    //...
-                                }
+                                //...
                             }
+                        }
 
-                    ){
-                        NewArcTextExample(path)
-                    }
+                    ) {
+                        val state = state.collectAsState(initial = null)
+                        state.value?.let {
+                            NewArcTextExample(it)
+                        }
+                    }*/
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DrawingApp() {
+
+    val context = LocalContext.current
+
+    /**
+     * Paths that are added, this is required to have paths with different options and paths
+     *  ith erase to keep over each other
+     */
+    val paths = remember { mutableStateListOf<Pair<androidx.compose.ui.graphics.Path, PathProperties>>() }
+
+    /**
+     * Paths that are undone via button. These paths are restored if user pushes
+     * redo button if there is no new path drawn.
+     *
+     * If new path is drawn after this list is cleared to not break paths after undoing previous
+     * ones.
+     */
+    val pathsUndone = remember { mutableStateListOf<Pair<androidx.compose.ui.graphics.Path, PathProperties>>() }
+
+    /**
+     * Canvas touch state. [MotionEvent.Idle] by default, [MotionEvent.Down] at first contact,
+     * [MotionEvent.Move] while dragging and [MotionEvent.Up] when first pointer is up
+     */
+    var motionEvent by remember { mutableStateOf(MotionEvent.Idle) }
+
+    /**
+     * Current position of the pointer that is pressed or being moved
+     */
+    var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
+
+    /**
+     * Previous motion event before next touch is saved into this current position.
+     */
+    var previousPosition by remember { mutableStateOf(Offset.Unspecified) }
+
+    /**
+     * Draw mode, erase mode or touch mode to
+     */
+    var drawMode by remember { mutableStateOf(DrawMode.Draw) }
+
+    /**
+     * Path that is being drawn between [MotionEvent.Down] and [MotionEvent.Up]. When
+     * pointer is up this path is saved to **paths** and new instance is created
+     */
+    var currentPath by remember { mutableStateOf(Path()) }
+
+    /**
+     * Properties of path that is currently being drawn between
+     * [MotionEvent.Down] and [MotionEvent.Up].
+     */
+    var currentPathProperty by remember { mutableStateOf(PathProperties()) }
+
+    val canvasText = remember { StringBuilder() }
+    val paint = remember {
+        android.graphics.Paint().apply {
+            textSize = 40f
+            color = Color.Black.toArgb()
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+
+        val drawModifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp)
+            .fillMaxWidth()
+            .weight(1f)
+            .background(Color.White)
+//            .background(getRandomColor())
+            .dragMotionEvent(
+                onDragStart = { pointerInputChange ->
+                    motionEvent = MotionEvent.Down
+                    currentPosition = pointerInputChange.position
+                    pointerInputChange.consumeDownChange()
+
+                },
+                onDrag = { pointerInputChange ->
+                    motionEvent = MotionEvent.Move
+                    currentPosition = pointerInputChange.position
+
+                    if (drawMode == DrawMode.Touch) {
+                        val change = pointerInputChange.positionChange()
+                        println("DRAG: $change")
+                        paths.forEach { entry ->
+                            val path: androidx.compose.ui.graphics.Path = entry.first
+                            path.translate(change)
+                        }
+                        currentPath.translate(change)
+                    }
+                    pointerInputChange.consumePositionChange()
+
+                },
+                onDragEnd = { pointerInputChange ->
+                    motionEvent = MotionEvent.Up
+                    pointerInputChange.consumeDownChange()
+                }
+            )
+
+        Canvas(modifier = drawModifier) {
+
+            when (motionEvent) {
+
+                MotionEvent.Down -> {
+                    if (drawMode != DrawMode.Touch) {
+                        currentPath.moveTo(currentPosition.x, currentPosition.y)
+                    }
+
+                    previousPosition = currentPosition
+
+                }
+                MotionEvent.Move -> {
+
+                    if (drawMode != DrawMode.Touch) {
+                        currentPath.quadraticBezierTo(
+                            previousPosition.x,
+                            previousPosition.y,
+                            (previousPosition.x + currentPosition.x) / 2,
+                            (previousPosition.y + currentPosition.y) / 2
+
+                        )
+                    }
+
+                    previousPosition = currentPosition
+                }
+
+                MotionEvent.Up -> {
+                    if (drawMode != DrawMode.Touch) {
+                        currentPath.lineTo(currentPosition.x, currentPosition.y)
+
+                        // Pointer is up save current path
+//                        paths[currentPath] = currentPathProperty
+                        paths.add(Pair(currentPath, currentPathProperty))
+
+                        // Since paths are keys for map, use new one for each key
+                        // and have separate path for each down-move-up gesture cycle
+                        currentPath = Path()
+
+                        // Create new instance of path properties to have new path and properties
+                        // only for the one currently being drawn
+                        currentPathProperty = PathProperties(
+                            strokeWidth = currentPathProperty.strokeWidth,
+                            color = currentPathProperty.color,
+                            strokeCap = currentPathProperty.strokeCap,
+                            strokeJoin = currentPathProperty.strokeJoin,
+                            eraseMode = currentPathProperty.eraseMode
+                        )
+                    }
+
+                    // Since new path is drawn no need to store paths to undone
+                    pathsUndone.clear()
+
+                    // If we leave this state at MotionEvent.Up it causes current path to draw
+                    // line from (0,0) if this composable recomposes when draw mode is changed
+                    currentPosition = Offset.Unspecified
+                    previousPosition = currentPosition
+                    motionEvent = MotionEvent.Idle
+                }
+                else -> Unit
+            }
+
+            with(drawContext.canvas.nativeCanvas) {
+
+                val checkPoint = saveLayer(null, null)
+
+                paths.forEach {
+
+                    val path = it.first
+                    val property = it.second
+
+                    if (!property.eraseMode) {
+                        drawIntoCanvas {
+                            it.nativeCanvas.drawTextOnPath("Hello World Example", path.asAndroidPath(), 0f, 0f, paint)
+                        }
+                        Log.e("TESTE", "HERE: ")
+                        /*drawPath(
+                            color = property.color,
+                            path = path,
+                            style = Stroke(
+                                width = property.strokeWidth,
+                                cap = property.strokeCap,
+                                join = property.strokeJoin
+                            )
+                        )*/
+                    } else {
+                        /*drawIntoCanvas {
+                            it.nativeCanvas.drawTextOnPath("Hello World Example", path.asAndroidPath(), 0f, 0f, paint)
+                        }*/
+                        // Source
+                        drawPath(
+                            color = Color.Transparent,
+                            path = path,
+                            style = Stroke(
+                                width = currentPathProperty.strokeWidth,
+                                cap = currentPathProperty.strokeCap,
+                                join = currentPathProperty.strokeJoin
+                            ),
+                            blendMode = BlendMode.Clear
+                        )
+                    }
+                }
+
+                if (motionEvent != MotionEvent.Idle) {
+
+                    if (!currentPathProperty.eraseMode) {
+                        drawPath(
+                            color = currentPathProperty.color,
+                            path = currentPath,
+                            style = Stroke(
+                                width = currentPathProperty.strokeWidth,
+                                cap = currentPathProperty.strokeCap,
+                                join = currentPathProperty.strokeJoin
+                            )
+                        )
+                    } else {
+                        drawPath(
+                            color = Color.Transparent,
+                            path = currentPath,
+                            style = Stroke(
+                                width = currentPathProperty.strokeWidth,
+                                cap = currentPathProperty.strokeCap,
+                                join = currentPathProperty.strokeJoin
+                            ),
+                            blendMode = BlendMode.Clear
+                        )
+                    }
+                }
+                restoreToCount(checkPoint)
+            }
+        }
+
+        DrawingPropertiesMenu(
+            modifier = Modifier
+                .padding(bottom = 8.dp, start = 8.dp, end = 8.dp)
+                .shadow(1.dp, RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(4.dp),
+            pathProperties = currentPathProperty,
+            drawMode = drawMode,
+            onUndo = {
+                if (paths.isNotEmpty()) {
+
+                    val lastItem = paths.last()
+                    val lastPath = lastItem.first
+                    val lastPathProperty = lastItem.second
+                    paths.remove(lastItem)
+
+                    pathsUndone.add(Pair(lastPath, lastPathProperty))
+
+                }
+            },
+            onRedo = {
+                if (pathsUndone.isNotEmpty()) {
+
+                    val lastPath = pathsUndone.last().first
+                    val lastPathProperty = pathsUndone.last().second
+                    pathsUndone.removeLast()
+                    paths.add(Pair(lastPath, lastPathProperty))
+                }
+            },
+            onPathPropertiesChange = {
+                motionEvent = MotionEvent.Idle
+            },
+            onDrawModeChanged = {
+                motionEvent = MotionEvent.Idle
+                drawMode = it
+                currentPathProperty.eraseMode = (drawMode == DrawMode.Erase)
+                Toast.makeText(
+                    context, "pathProperty: ${currentPathProperty.hashCode()}, " +
+                            "Erase Mode: ${currentPathProperty.eraseMode}", Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
     }
 }
 
@@ -107,8 +426,8 @@ fun ArcTextExample() {
 
         drawIntoCanvas {
             val path = Path()
-            path.addArc(RectF(0f, 100f, 200f, 300f), 270f, 180f)
-            it.nativeCanvas.drawTextOnPath("Hello World Example", path, 0f, 0f, paint)
+//            path.addArc(RectF(0f, 100f, 200f, 300f), 270f, 180f)
+//            it.nativeCanvas.drawTextOnPath("Hello World Example", path, 0f, 0f, paint)
         }
     }
 }
@@ -125,7 +444,7 @@ fun NewArcTextExample(path: Path) {
         }
 
         drawIntoCanvas {
-            it.nativeCanvas.drawTextOnPath("Hello World Example", path, 0f, 0f, paint)
+            it.nativeCanvas.drawTextOnPath("Hello World Example", path.asAndroidPath(), 0f, 0f, paint)
         }
     }
 }
---
+++ app/src/main/java/com/renatoocorrea/textbrush/PathProperties.kt
@@ -0,0 +1,14 @@
+package com.renatoocorrea.textbrush
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+
+class PathProperties(
+    var strokeWidth: Float = 10f,
+    var color: Color = Color.Black,
+    var alpha: Float = 1f,
+    var strokeCap: StrokeCap = StrokeCap.Round,
+    var strokeJoin: StrokeJoin = StrokeJoin.Round,
+    var eraseMode: Boolean = false
+)
